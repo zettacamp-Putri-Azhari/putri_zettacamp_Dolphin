07579304b49a89549deec67cfefc26d3e2b9c156
zettacamp-Putri-Azhari <putri.azhari@zettacamp.pro>
2023-08-25 18:13:17 +0700

    Declare Variabel

---
+++ DAY9-JS-DeclareVariabel/book.js
@@ -0,0 +1,21 @@
+// Declare two variable with name of your favourite books
+// Update value of first with name your least favourite book
+let FavouriteBook = 'Amorfati';
+console.log(FavouriteBook);
+
+FavouriteBook = true;
+console.log(FavouriteBook);
+FavouriteBook = 'Kala';
+console.log(FavouriteBook);
+
+// Define second variable as constant and try update the value
+const LeastFavouriteBook = 'Duduk Dulu';
+//LeastFavouriteBook = 'Adipati';
+console.log(LeastFavouriteBook);
+
+// Concat value from variables task number 1 into new variable
+let MyFavbooks = FavouriteBook + ' ' + LeastFavouriteBook;
+console.log(MyFavbooks);
+
+// Note:
+// Try declare more variable using other data type with dynamic value and primitive value
